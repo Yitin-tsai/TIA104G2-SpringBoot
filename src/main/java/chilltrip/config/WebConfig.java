a3a705b4c711bf2d0bb713fce6a9f23f341c8358
Hsiu-chiLiu <l224762938@gmail.com>
2025-01-04 14:41:03 +0800

    靜態資源綁定+把頁面交給thymeleaf管理（骯變實現日後更複雜的轉跳，以及大中小編輯器的切換）

--- src/main/java/chilltrip/config/WebConfig.java
+++ src/main/java/chilltrip/config/WebConfig.java
@@ -1,13 +1,15 @@
 package chilltrip.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/templates/frontend/js/");
-    }
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/**")
+	                .addResourceLocations("classpath:/static/");
+	    }
 }
